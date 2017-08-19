88553d5affbbd7c0bb8452c099f894da0fa13fed
Joshua Comeau <joshwcomeau@gmail.com>
2017-08-19 14:31:41 -0400

    Start implementing google auth

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,20 +1,17 @@
 const nconf = require('nconf');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const { MongoClient } = require('mongodb');
 
-// load the auth variables
-var configAuth = require('./auth');
+const databaseConnection = require('../db');
 
-module.exports = function(passport) {
-
+module.exports = (passport) => {
   // used to serialize the user for the session
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
+  passport.serializeUser((user, done) => done(null, user.id));
 
   // used to deserialize the user
   passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
+    databaseConnection.then(db => {
+      db.collection('users').findOne({ _id: id }, done)
     });
   });
 
@@ -22,41 +19,59 @@ module.exports = function(passport) {
   // GOOGLE ==================================================================
   // =========================================================================
   passport.use(new GoogleStrategy({
-    clientID        : nconf.get('GOOGLE_CLIENT_ID'),
-    clientSecret    : nconf.get('GOOGLE_CLIENT_SECRET'),
-    callbackURL     : nconf.get('GOOGLE_CALLBACK_URL'),
+    clientID: nconf.get('GOOGLE_CLIENT_ID'),
+    clientSecret: nconf.get('GOOGLE_CLIENT_SECRET'),
+    callbackURL: nconf.get('GOOGLE_CALLBACK_URL'),
   }, (token, refreshToken, profile, done) => {
+    console.log("PASSPORT GOT STUFF", profile);
     // make the code asynchronous
     // User.findOne won't fire until we have all our data back from Google
-    process.nextTick(function() {
+    process.nextTick(() => {
+      databaseConnection.then(db => {
+        const query = { 'google.id': profile.id };
 
-        // try to find the user based on their google id
-        User.findOne({ 'google.id' : profile.id }, function(err, user) {
-          if (err)
+        db.collection('users').findOne(query, (err, user) => {
+          if (err) {
             return done(err);
-
-          if (user) {
-
-            // if a user is found, log them in
-            return done(null, user);
-          } else {
-            // if the user isnt in our database, create a new user
-            var newUser          = new User();
-
-            // set all of the relevant information
-            newUser.google.id    = profile.id;
-            newUser.google.token = token;
-            newUser.google.name  = profile.displayName;
-            newUser.google.email = profile.emails[0].value; // pull the first email
-
-            // save the user
-            newUser.save(function(err) {
-              if (err)
-                throw err;
-              return done(null, newUser);
-            });
           }
+
+          // if a user is found, log them in
+          if (user) {
+            return done(null, user);
+          }
+
+          // If no user is found, create a new user.
+          console.log('PROFILE', profile);
+        });
       });
+
+      // // try to find the user based on their google id
+      // User.findOne({ 'google.id' : profile.id }, function(err, user) {
+      //   if (err)
+      //     return done(err);
+      //
+      //   if (user) {
+      //
+      //     // if a user is found, log them in
+      //     return done(null, user);
+      //   } else {
+      //     // if the user isnt in our database, create a new user
+      //     var newUser          = new User();
+      //
+      //     // set all of the relevant information
+      //     newUser.google.id    = profile.id;
+      //     newUser.google.token = token;
+      //     newUser.google.name  = profile.displayName;
+      //     newUser.google.email = profile.emails[0].value; // pull the first email
+      //
+      //     // save the user
+      //     newUser.save(function(err) {
+      //       if (err)
+      //         throw err;
+      //       return done(null, newUser);
+      //     });
+      //   }
+      // });
     });
 
   }));
--- server/index.js
+++ server/index.js
@@ -8,26 +8,43 @@ const passport = require('passport');
 
 require('./initialize');
 
+
+const databaseConnection = require('./db');
+
+
 const app = express();
 
 app.set('port', nconf.get('PORT') || 3005);
 
+
+require('./config/passport')(passport);
+
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope : ['profile', 'email'] })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect : '/',
+    failureRedirect : '/'
+  })
+);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
 }
 
-app.get('/random-photo', (req, res) => {
-  const dimensions = `${req.query.size}x${req.query.size}`
-  const url = `https://source.unsplash.com/random/${dimensions}`;
-
-  request(url).pipe(res);
-});
-
-app.listen(app.get('port'), () => {
-  console.info(`==> 🌎  Listening on port ${app.get('port')}.`);
+databaseConnection.then(db => {
+  app.listen(nconf.get('PORT'), () => {
+    console.info(`==> 🌎  Listening on port ${nconf.get('PORT')}.`);
+  });
+}).catch(err => {
+  console.error('Error connecting to database', err);
 });
