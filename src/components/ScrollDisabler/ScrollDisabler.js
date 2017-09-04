1ca413ba9ad6233e88785388ee883f37bc82749a
Joshua Comeau <joshwcomeau@gmail.com>
2017-09-04 10:29:41 -0400

    Fix bug with ScrollDisabler

--- src/components/ScrollDisabler/ScrollDisabler.js
+++ src/components/ScrollDisabler/ScrollDisabler.js
@@ -11,8 +11,18 @@ class ScrollDisabler extends PureComponent {
   }
   componentDidMount() {
     this.oldOverflow = document.body.style.overflow;
+    this.oldPosition = document.body.style.position;
+    this.oldWidth = document.body.style.width;
+    this.oldHeight = document.body.style.height;
+    this.oldTop = document.body.style.top;
+
+    this.oldScrollY = window.scrollY;
 
     document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
+    document.body.style.height = `calc(100% + ${this.oldScrollY}px)`;
+    document.body.style.top = `-${this.oldScrollY}px`;
 
     if (this.props.applyLightScrollTheme) {
       document.body.classList.add('light-scroll');
@@ -21,6 +31,13 @@ class ScrollDisabler extends PureComponent {
 
   componentWillUnmount() {
     document.body.style.overflow = this.oldOverflow;
+    document.body.style.position = this.oldPosition;
+    document.body.style.width = this.oldWidth;
+    document.body.style.height = this.oldHeight;
+    document.body.style.top = this.oldTop;
+
+    window.scrollTo(0, this.oldScrollY)
+
     document.body.classList.remove('light-scroll');
   }
 
