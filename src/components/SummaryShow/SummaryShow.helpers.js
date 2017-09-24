038f94dcc1513ed65fb2d373783bb42f2f7e26e2
Joshua Comeau <joshwcomeau@gmail.com>
2017-09-23 20:07:07 -0400

    Ensure rounded pixel display ratios, since Imgix needs this

--- src/components/SummaryShow/SummaryShow.helpers.js
+++ src/components/SummaryShow/SummaryShow.helpers.js
@@ -3,5 +3,5 @@ const dpr = window.devicePixelRatio;
 const defaultImage = 'https://tello.imgix.net/placeholder.jpg';
 
 export const buildImageUrl = ({ image, width, height }) => (`
-  ${image || defaultImage}?fit=crop&crop=entropy&h=${height}&w=${width}&auto=enhance&dpr=${dpr} ${width * dpr}w
+  ${image || defaultImage}?fit=crop&crop=entropy&h=${height}&w=${width}&auto=enhance&dpr=${Math.round(dpr)} ${Math.round(width * dpr)}w
 `);
