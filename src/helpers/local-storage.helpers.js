2b8a616c1745a7549581953773cd8b7abbd568a0
Joshua Comeau <joshwcomeau@gmail.com>
2017-09-24 16:08:34 -0400

    Simplify localstorage helpers

--- src/helpers/local-storage.helpers.js
+++ src/helpers/local-storage.helpers.js
@@ -1,12 +1,16 @@
 import { LOCAL_STORAGE_REDUX_DATA_KEY } from '../constants';
-import { isEmpty, generateDebouncedLocalStorageSetter } from '../utils';
+import { debounce } from '../utils';
 
 
 // When our page first loads, a bunch of redux actions are dispatched rapidly
 // (each show needs to request and then receive their episodes, so the minimum
 // number of actions is 2n, where `n` is the number of shows).
 // We don't need to update localStorage _that_ often, so let's debounce it.
-const updateLocalStorage = generateDebouncedLocalStorageSetter(2500);
+const updateLocalStorage = debounce(
+  (...args) => localStorage.setItem(...args),
+  2500
+);
+
 
 // Whenever the store updates, store relevant parts in localStorage.
 // This way, we can initialize the store on page refresh, and have the data
--- src/utils/index.js
+++ src/utils/index.js
@@ -121,11 +121,6 @@ export function setTimeoutPromise(duration) {
   ));
 }
 
-export const generateDebouncedLocalStorageSetter = duration => debounce(
-  (...args) => { localStorage.setItem(...args) },
-  duration
-);
-
 export const deleteCookie = (key) => {
   document.cookie = `${encodeURIComponent(key)}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
 }
