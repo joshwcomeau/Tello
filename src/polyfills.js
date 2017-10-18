be3dd8566b35a4e041adad1aade81439940974f7
Joshua Comeau <joshwcomeau@gmail.com>
2017-10-18 07:01:21 -0400

    Add Object#entries polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -69,3 +69,15 @@ if (!Array.prototype.includes) {
     }
   });
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries#Polyfill
+if (!Object.entries)
+  Object.entries = function (obj) {
+    var ownProps = Object.keys(obj),
+      i = ownProps.length,
+      resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+    return resArray;
+  };
