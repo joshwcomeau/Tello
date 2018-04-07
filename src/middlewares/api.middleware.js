783ec10668888dbeac3892722a0fd998e6742c6a
Josh Comeau <joshua@khanacademy.org>
2018-04-07 14:31:36 -0400

    Try to debug user issue

--- src/middlewares/api.middleware.js
+++ src/middlewares/api.middleware.js
@@ -34,6 +34,10 @@ export default function createAPIMiddleware() {
     // must be logged out, and shouldn't be interacting with our API.
     const token = Cookies.get(AUTH_TOKEN_KEY);
 
+    if (!token) {
+      return;
+    }
+
     // We may want to show some of the UI components on splash pages, as demo
     // units. In those cases we don't want to make any network requests.
     // We need to "fake" it here instead
@@ -45,6 +49,7 @@ export default function createAPIMiddleware() {
       case USER_DATA_REQUEST: {
         getAuthUserData({ token })
           .then(json => {
+            console.log('Got JSON', json);
             next(userDataReceive(json));
           })
           .catch(err => {
